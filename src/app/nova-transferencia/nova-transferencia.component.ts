18764c722ebd10b7a9ac0eeed9ec8357a5582f2c
unknown <gabeclementej@gmail.com>
2022-05-24 16:44:07 -0300

    feat newtransfer component

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,39 +1,34 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { Router } from '@angular/router';
-import { Transferencia } from '../models/transferencia.model';
-import { TransferenciaService } from '../services/transferencia.service';
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { Transferencia } from "../models/transferencia.model";
+import { TransferenciaService } from "../services/transferencia.service";
 
 @Component({
-  selector: 'app-nova-transferencia',
-  templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss'],
+    selector:'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
 })
-export class NovaTransferenciaComponent{
 
-  @Output() aoTransferir =  new EventEmitter<any>();
+export class NovaTransgerenciaComponent {
+    @Output() aoTransferir = new EventEmitter<any>();
 
-  valor: number = 0;
-  destino: number = 0;
+    valor: number;
+    destino: number;
 
-  constructor(private service: TransferenciaService, private router: Router){
+    constructor(
+        private transferenciaService: TransferenciaService,
+        private router: Router
+    ) {}
 
-  }
-
-  transferir(){
-    const valorEmitir:Transferencia = {valor: this.valor, destino: this.destino}
-
-    this.service.adicionar(valorEmitir).subscribe(resultado =>{
-      this.limparCampos();
-      this.router.navigateByUrl('extrato');
-    }, (error) =>{
-      console.error(error)
-    });
-
-    this.limparCampos();
-  }
-
-  limparCampos(){
-    this.valor = 0
-    this.destino = 0
-  }
+    novaTransferencia() {
+        const novaTransferencia: Transferencia = {
+            valor: this.valor,
+            destino: this.destino
+        }
+        this.transferenciaService.adicionar(novaTransferencia).subscribe((resultado) => {
+            console.log(resultado);
+            this.router.navigateByUrl('extrato');
+        },
+        error => console.log(error));
+    }
 }
