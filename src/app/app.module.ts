777693e8b2856e2258290b9ae584b0f598396be0
unknown <gabeclementej@gmail.com>
2022-05-24 16:45:51 -0300

    feat module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { NovaTransgerenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
 import localePt from '@angular/common/locales/pt';
@@ -13,8 +13,14 @@ import { AppRoutingModule } from './app-routing.module';
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
-  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  declarations: [AppComponent, NovaTransgerenciaComponent, ExtratoComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
     {
