fc075114c84587ff3050b4ee806cdbf754c6dd51
unknown <gabeclementej@gmail.com>
2022-05-24 16:46:28 -0300

    feat service

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,31 +1,33 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Transferencia } from '../models/transferencia.model';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Transferencia } from '../models/transferencia.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TransferenciaService {
   private listaTransferencia: any[];
-  private url = "http://localhost:3000/transferencias";
+  private url = 'http://localhost:3000/transferencias';
 
   constructor(private httpClient: HttpClient) {
     this.listaTransferencia = [];
   }
 
-  get transferencias(){
+  get transferencias() {
     return this.listaTransferencia;
   }
-  todas(): Observable<Transferencia[]>{
-    return this.httpClient.get<Transferencia[]>(this.url)
+
+  todas(): Observable<Transferencia[]> {
+    return this.httpClient.get<Transferencia[]>(this.url);
   }
+
   adicionar(transferencia: Transferencia): Observable<Transferencia> {
     this.hidratar(transferencia);
     return this.httpClient.post<Transferencia>(this.url, transferencia);
   }
 
-  private hidratar(transferencia:any){
+  private hidratar(transferencia: any) {
     transferencia.data = new Date();
   }
 }
